e4166cbee584e329dc4b245dd838ff754713a4bf
adminappmakeregypt <admin@appmakeregypt.com>
2026-06-28 15:26:42 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 // ============ Firebase shared config ============
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
